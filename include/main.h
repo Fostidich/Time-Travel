dc9c7baa077bcb9cdb7e103bb6a63be3a33cdc93
Fostidich <francesco.ostidich@mail.polimi.it>
2024-04-04 15:35:27 +0200

    first commit

---
+++ include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by kello on 04/04/24.
+//
+
+#ifndef TIME_TRAVEL_MAIN_H
+#define TIME_TRAVEL_MAIN_H
+
+int main();
+
+#endif //TIME_TRAVEL_MAIN_H
---
+++ src/main.c
@@ -0,0 +1,11 @@
+//
+// Created by kello on 04/04/24.
+//
+
+#include <stdio.h>
+#include "../include/main.h"
+
+int main() {
+    printf("HELLO!!!");
+    return 0;
+}
