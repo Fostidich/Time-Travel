d9233a45293da5a9c1d12662458b41322bf89752
Fostidich <francesco.ostidich@mail.polimi.it>
2024-04-06 18:41:12 +0200

    main may be done hopefully

--- src/main.c
+++ src/main.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
             continue;
 
         size_t filename_length = strnlen(entry->d_name, sizeof(entry->d_name) - 1);
-        char filename[filename_length + 1];
+        char filename[filename_length + 16];
         strncpy(filename, entry->d_name, filename_length);
         filename[filename_length] = '\0';
         if (filename_length == sizeof(entry->d_name) - 1) {
@@ -52,15 +52,15 @@ int main(int argc, char **argv) {
         int has_extension = first_dot != NULL && first_dot != filename + filename_length - 1;
         if (has_extension) {
             extension_length = filename + filename_length - first_dot + 1;
-            strncpy(extension, first_dot, extension_length);
+            strcpy(extension, first_dot);
             *first_dot = '\0';
         }
 
-        char new_name[DATE_LEN + extension_length];
+        char new_name[DATE_LEN + extension_length + 16];
         const int outcome = findDate(new_name, filename);
         if (has_extension) {
-            strncpy(first_dot, extension, extension_length);
-            strncpy(new_name + DATE_LEN, extension, extension_length);
+            strcpy(first_dot, extension);
+            strcpy(new_name + DATE_LEN, extension);
         }
 
         if (outcome == DATE_UNKNOWN) {
@@ -83,13 +83,16 @@ int main(int argc, char **argv) {
                 if (has_extension) {
                     char *last_dot = strrchr(new_path, '.');
                     snprintf(temp, PATH_MAX, "%.*s (%d)%s",
-                             (int)(last_dot - new_path), new_path, duplicates, extension);
+                             (int) (last_dot - new_path), new_path, duplicates, extension);
                 } else {
                     snprintf(temp, PATH_MAX, "%.4000s (%d)", new_path, duplicates);
                 }
                 duplicates++;
             } while (access(temp, F_OK) == 0);
             strcpy(new_path, temp);
+            has_extension ?
+            sprintf(new_name + DATE_LEN, " (%d)%s", duplicates, extension) :
+            sprintf(new_name + DATE_LEN, " (%d)", duplicates);
         }
 
         if (rename(old_path, new_path) != 0) {
@@ -98,11 +101,9 @@ int main(int argc, char **argv) {
         }
 
         changed++;
-        if (outcome == DATE_UNSURE) {
-            printf("%s%s -> %s%s\n", YELLOW, filename, new_name, WHITE);
-        } else {
-            printf("%s -> %s\n", filename, new_name);
-        }
+        outcome == DATE_UNSURE ?
+        printf("%s%s -> %s%s\n", YELLOW, filename, new_name, WHITE) :
+        printf("%s -> %s\n", filename, new_name);
     }
 
     if (changed == 0) {
