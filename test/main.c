f4c0d292b28c63d663de1e60ba85a34644114f4d
Fostidich <francesco.ostidich@mail.polimi.it>
2024-08-10 15:44:57 +0200

    test executable (but test directory unable to be opened)

--- test/main.c
+++ test/main.c
@@ -1,12 +1,94 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "finder.h"
 
-/// Argv contains, in order, input filename string, expected found string,
-/// outcome int value
+#define BUF_LEN 256
+
+int test_line(const char *line);
+int test_check(int outcome, const char *expected, const char *source);
+int extract_from_line(int *outcome, char *expected, char *source, const char *buffer);
+
 int main(int argc, char **argv) {
-    char result[256];
-    int outcome = find_date(result, argv[0]);
-    return !(strcmp(argv[1], result) == 0 && outcome == atoi(argv[2]));
+    FILE *file;
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Unable to open the provided file");
+        return 0;
+    }
+
+    char buffer[BUF_LEN];
+    int count = 0;
+    int fails = 0;
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        count++;
+        switch (test_line(buffer)) {
+            case -1:
+                fprintf(stderr, "\033[33m!\033[0m %d: unable to read line", count);
+                fails++;
+                continue;
+            case 0:
+                printf("\033[31mx\033[0m %d: %s", count, buffer);
+                fails++;
+                continue;
+            case 1:
+                printf("\033[32mv\033[0m %d: %s", count, buffer);
+                continue;
+        }
+    }
+
+    fclose(file);
+    return !fails;
+}
+
+int test_line(const char *line) {
+    int outcome;
+    char expected[BUF_LEN];
+    char source[BUF_LEN];
+    if (!extract_from_line(&outcome, expected, source, line)) return -1;
+    return test_check(outcome, expected, source);
+}
+
+int test_check(int outcome, const char *expected, const char *source) {
+    char result[BUF_LEN];
+    int state = find_date(result, source);
+    return !(strcmp(expected, result) == 0 && outcome == state);
+}
+
+int extract_from_line(int *outcome, char *expected, char *source, const char *buffer) {
+    int i = 0, j = 0;
+
+    while (buffer[i++] == ' ');
+    switch (buffer[i++]) {
+        case 0:
+            *outcome = 0;
+            break;
+        case 1:
+            *outcome = 1;
+            break;
+        case 2:
+            *outcome = 2;
+            break;
+        case 3:
+            *outcome = 3;
+            break;
+        case 4:
+            *outcome = 4;
+            break;
+        default:
+            return -1;
+    }
+
+    if (buffer[i++] != ' ') return 0;
+    while (buffer[i++] == ' ');
+    while (buffer[i++] != ' ') expected[j++] = buffer[i];
+    expected[j] = '\0';
+    j = 0;
+
+    if (buffer[i++] != ' ') return 0;
+    while (buffer[i++] == ' ');
+    while (buffer[i++] != ' ') source[j++] = buffer[i];
+    source[j] = '\0';
+    return 1;
 }
